ba78fde2504d811938ceec8ecdafc5bd1006548e
Martin <martin.lippl@pueo.de>
2018-07-21 20:24:57 +0200

    fused publisher and shell

--- ROS/command.py
+++ ROS/command.py
@@ -26,6 +26,49 @@ class Command:
 			r.sleep()
 
 
+# fixed rpm of motor
+motorRPM = 80
+
+# circumference of tire: 2*PI*tireRadius or PI*diameter
+tireDiameter = 0.
+tireCirc = PI * tireDiameter
+
+
+
+def forward(length):
+	rounds = length / tireCirc
+	time = rounds / motorRPM
+	return time, motorRPM
+
+
+def backward(length):
+	rounds = length / tireCirc
+	time = rounds / motorRPM 
+	return time, motorRPM
+
+def left(deg):
+	pass
+
+
+
+def right(deg):
+	pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__=='__main__':
 	try:
--- ROS/shell.py
+++ ROS/shell.py
@@ -1,39 +1,102 @@
-import cmd
-# also import the robot's-functionality library!
-# [...]
+####################################################################
+#
+#   Node which requests commands and publishes on a twist topic
+#
+####################################################################
+
+import cmd, sys
+import math
+
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+
 
 # Helper function
-def parse():
-    pass
+def parse(arg):
+    #print (tuple(map(float, arg.split())))
+    return tuple(map(float, arg.split()))
 
 
+# ROS backend
+class Publisher():
+    def __init__(self):
+        rospy.init_node('robbie_console', anonymous=False)
+        self.publisher = rospy.Publisher('command', String, queue_size=10)
+
+        # Default values:
+        self.motorRPM = 80
+        self.tireDiameter = 5. # circumference of tire: 2*PI*tireRadius or PI*diameter
+        self.tireCirc = math.pi * self.tireDiameter
+
+    def forward(self, length):
+        print("Move forward by %3.1f cm." % length[0])
+        rounds = length[0] / self.tireCirc
+        time = rounds / self.motorRPM
+        #return time, motorRPM
+        self.publisher.publish('AfD')
+
+    def backward(self, length):
+        print("Move backward by %3.1f cm." % length[0])
+        #rounds = length / tireCirc
+        #time = rounds / motorRPM 
+        #return time, motorRPM
+
+    def left(self, deg):
+        print("Turn left")
+        pass
+
+    def right(self, deg):
+        print("Turn right")
+        pass
+
+    def quit(self):
+        print('Shutting down node \'robbie_console\'...')
+        rospy.signal_shutdown('Manual shutdown of node via Robbie\'s Shell.')
+        #raise SystemExit()
+
+
+
+
+# Frontend
 class Shell(cmd.Cmd):
-    intro = 'Welcome to Robbie\'s shell. Type \'help\' or \'?\' to list commands.\n'
-    prompt = '(robbie)'
-    file = None # Meaning of this?
+    def __init__(self):
+        super(Shell, self).__init__()
+        self.prompt = '(robbie) $ '
+        self.intro = 'Welcome to Robbie\'s Shell. Enter \'help\' or \'?\' to list commands.\n'
+        self.file = None # Later for waypoint files...
+        self.pub = Publisher()
 
     # Basic commands:
-
     def do_forward(self, arg):
         'Move forward by the specified distance [cm]: FORWARD 10'
-        forward(*parse(arg))
+        #forward(*parse(arg))
+        self.pub.forward(parse(arg))
 
     def do_backward(self, arg):
         'Move backward by the specified distance [cm]: BACKWARD 10'
-        backward(*parse(args))
+        self.pub.backward(parse(arg))
 
     def do_left(self, arg):
         'Turn left by the specified angle [degree]: LEFT 45'
-        left(*parse(arg))
+        self.pub.left(parse(arg))
 
     def do_right(self, arg):
         'Turn right by the specified angle [degree]: RIGHT 45'
-        right(*parse(arg))
+        self.pub.right(parse(arg))
 
-
+    def do_quit(self, arg):
+        'Quit shell and shutdown node.'
+        self.pub.quit()
+        raise SystemExit()
 
 
 
 
 if __name__=='__main__':
     Shell().cmdloop()
+
+
+
+
