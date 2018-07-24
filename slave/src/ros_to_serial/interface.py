86893f460fc952dc61ecbbb8a1a639d4a5741380
Martin <martin.lippl@pueo.de>
2018-07-24 22:23:07 +0200

    devided catkin project into master and slave

--- slave/src/ros_to_serial/interface.py
+++ slave/src/ros_to_serial/interface.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #################################################################################
 #
 #	Subscribes to twist topic and publishes them via serial port to the MCU
@@ -6,27 +8,33 @@
 
 import rospy
 from std_msgs.msg import String
-from geometry_msgs.msg import Twist
+from robbie_msgs.msg import command
 import serial
 
 SERIAL_PORT = '/dev/ttyACM0'
 
 
+
+
+
 class Controller:
 	def __init__(self):
 		rospy.init_node('motor_interface', anonymous=True)
-		# Subscribt to commands from command node
-		subscriber = rospy.Subscriber('/command')
+		# Subscribe to commands from command node:
+		subscriber = rospy.Subscriber('/command', command, self.motor_command)
 		# Serial port to publish motor control signals to
 		# Lower boud rate to publish motor commands might be appropriate
-		self.ser = serial.Serial('/dev/ttyACM0', 115200)
+		#self.ser = serial.Serial('/dev/ttyACM0', 115200)
 		rospy.spin()
 
-	def motor_command():
-		left = pwm()
-		right = pwm()
-		command = '{},{}'.format(left, right)
-		self.ser.write(command)
+	def motor_command(self, cmd_msg):
+		duration = cmd_msg.duration
+		rpm_left = cmd_msg.rpm_left
+		rpm_right = cmd_msg.rpm_right
+
+		print("%3.5f, %d, %d" % (duration, rpm_left, rpm_right))
+		#command = '{},{},{}'.format(duration, rpm_left, rpm_right)
+		#self.ser.write(command)
 
 
 	"""
@@ -39,9 +47,9 @@ class Controller:
 	"""
 
 
-if __name__=='__main__':
-	print("Starting Motor Interface node")
-	try:
-		c = Controller()
-	except rospy.ROSInterruptException:
-		pass
+if __name__ == '__main__':
+	print("Starting Motor Interface node...")
+	#try:
+	c = Controller()
+	#except rospy.ROSInterruptException:
+	#	pass
